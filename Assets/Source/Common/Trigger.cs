ca67f1c8170480479eea2cb16310b499e9365d9b
Quinton <quinncg.dev@gmail.com>
2025-07-31 23:01:15 -0400

    Basic button puzzle

--- Assets/Source/Common/Trigger.cs
+++ Assets/Source/Common/Trigger.cs
@@ -7,13 +7,32 @@ namespace Quinn
 	public class Trigger : MonoBehaviour
 	{
 		[SerializeField]
-		private UnityEvent OnTrigger;
+		private UnityEvent OnEnter, OnExit;
+		[SerializeField]
+		private bool TriggerOnce;
+
+		private bool _hasTriggered;
 
 		public void OnTriggerEnter2D(Collider2D collision)
 		{
+			if (_hasTriggered && TriggerOnce)
+				return;
+
 			if (collision.IsPlayer())
 			{
-				OnTrigger?.Invoke();
+				OnEnter?.Invoke();
+			}
+		}
+
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			if (_hasTriggered && TriggerOnce)
+				return;
+
+			if (collision.IsPlayer())
+			{
+				_hasTriggered = true;
+				OnExit?.Invoke();
 			}
 		}
 	}
